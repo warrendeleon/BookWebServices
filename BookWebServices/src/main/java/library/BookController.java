ca4700a17962fd1811aed5c1c3335d29e705d65b
Warren de León Ofalla <warren.deleon@differentthings.es>
2014-10-08 04:55:22 +0200

    Comment deleted

--- BookWebServices/src/main/java/library/BookController.java
+++ BookWebServices/src/main/java/library/BookController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 
+	
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/search")
